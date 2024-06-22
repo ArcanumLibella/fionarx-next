3655fb37a7b73c40761b98d68e7f8f14dbc3db2d
ArcanumLibella <fiona.roux@hetic.net>
2024-06-22 16:29:57 +0200

    feat: fetch Hero data from API

--- app/_components/organisms/Hero/Hero.jsx
+++ app/_components/organisms/Hero/Hero.jsx
@@ -1,37 +1,32 @@
 import React from 'react';
+import axios from "axios";
 import { Blobs } from "../Blobs";
-// import { Heading } from "../../atoms/Heading";
+import Link from "next/link";
+import { Heading } from "../../atoms/Heading";
 
-export const Hero = () => {
-  // const { strapiGlobal } = useStaticQuery(graphql`
-  //   query {
-  //     strapiGlobal {
-  //       hero {
-  //         label
-  //         link
-  //       }
-  //     }
-  //   }
-  // `);
+export const Hero = async () => {
+  const response = await axios.get("http://127.0.0.1:1337/api/global?populate=deep");
+  console.log("response : ", response.data.data.attributes.hero)
+  const heroItems = response.data.data.attributes.hero;
 
-  // const renderHero = strapiGlobal.hero.map((item, index) => {
-  //   if (item.link) {
-  //     return (
-  //       <Link rel="canonical" to={item.link} className="z-10 no-underline">
-  //         <h1 className="text-[12vw] font-black uppercase cursor-pointer select-none md:text-[10vw] lg-text-[10vh] xl:text-[12vh] font-brother text-white">
-  //           {item.label}
-  //         </h1>
-  //       </Link>
-  //     )
-  //   } else {
-  //     return <Heading key={index} level={2} className="font-brother font-black uppercase mb-4 xl:mb-0 select-none !text-[8vw] lg:!text-[6vh] xl:!text-[8vh] !tracking-normal text-shadow text-stroke-purple-ultraLight md:text-stroke-2 md:text-fill-transparent opacity-20 md:opacity-40">{item.label}</Heading>
-  //   }
-  // })
+  const renderHero = heroItems.map((item) => {
+    if (item.link) {
+      return (
+        <Link href={item.link} key={item.id} rel="canonical" className="z-10 no-underline">
+          <h1 className="text-[12vw] font-black uppercase cursor-pointer select-none md:text-[10vw] lg-text-[10vh] xl:text-[12vh] font-brother text-white">
+            {item.label}
+          </h1>
+        </Link>
+      )
+    } else {
+      return <Heading key={item.id} level={2} className="font-brother font-black uppercase mb-4 xl:mb-0 select-none !text-[8vw] lg:!text-[6vh] xl:!text-[8vh] !tracking-normal text-shadow text-stroke-purple-ultraLight md:text-stroke-2 md:text-fill-transparent opacity-20 md:opacity-40">{item.label}</Heading>
+    }
+  })
 
   return (
     <div className="flex flex-col items-center justify-center h-screen min-h-screen overflow-hidden text-center px-4 md:pl-16 md:pr-8 lg:px-16 lg:py-16 md:items-end md:text-right bg-midnight">
       <Blobs className="absolute flex items-center justify-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[56vw] h-[36vh] lg:w-[48vw] lg:h-[54vh] xl:w-[32vw] xl:h-[56vh]" />
-      {/* {renderHero} */}
+      {renderHero}
     </div>
   );
 };
