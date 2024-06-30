b34eaffe4c6932808463cdd2f782851f11149bac
ArcanumLibella <fiona.roux@hetic.net>
2024-06-30 16:19:42 +0200

    fix: try CategoryPage optimization

--- app/categories/[categorie]/page.jsx
+++ app/categories/[categorie]/page.jsx
@@ -3,48 +3,54 @@ import { fetchDataFromStrapi } from "@/app/_utils/strapi.utils";
 import { NoContent, ProjectCard, ProjectsByCategoryLayout } from "@/app/_components";
 import NotFoundPage from "@/app/404";
 
-
 export default async function ProjectsByCategoryPage({ params }) {
   const { categorie: slug } = params;
   let categorie;
-  
+
   try {
     const categories = await fetchDataFromStrapi("categories?populate=deep");
-    categorie = categories.find((categorie) => categorie.attributes.slug === slug);
+    if (categories && categories.length > 0) {
+      categorie = categories.find((categorie) => categorie?.attributes?.slug === slug);
+    }
   } catch (error) {
     console.error("Error fetching categories", error);
-  }
-
-  if (!categorie) {
     return <NotFoundPage />;
   }
 
-  const { name, projects } = categorie.attributes || { name: "", projects: { data: [] } };
-  const noContent = projects.data.length === 0;
+  if (!categorie || !categorie.attributes) {
+    return <NotFoundPage />;
+  }
+
+  const { name, projects } = categorie.attributes;
+  const noContent = !projects?.data || projects.data.length === 0;
 
   return (
     <ProjectsByCategoryLayout name={name} noContent={noContent}>
       {noContent ? (
         <NoContent />
       ) : (
-        projects?.data?.map((project) => (
-          <ProjectCard
-            key={project.id}
-            title={project.attributes.title}
-            missions={project.attributes.missions}
-            slug={project.attributes.slug}
-            thumbnail={project.attributes.thumbnail.data.attributes}
-          />
-        ))
+        projects.data.map((project) => {
+          const projectAttributes = project?.attributes || {};
+          const thumbnail = projectAttributes.thumbnail?.data?.attributes || null;
+
+          return (
+            <ProjectCard
+              key={project.id}
+              title={projectAttributes.title}
+              missions={projectAttributes.missions}
+              slug={projectAttributes.slug}
+              thumbnail={thumbnail}
+            />
+          );
+        })
       )}
     </ProjectsByCategoryLayout>
-  )
+  );
 }
 
 export async function generateStaticParams() {
   try {
     const categories = await fetchDataFromStrapi("categories");
-
     return categories.map((categorie) => ({
       categorie: categorie.attributes.slug,
     }));
