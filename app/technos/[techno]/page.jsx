aedaffcfec46b8424b58e66b5c74571cbad6828f
ArcanumLibella <fiona.roux@hetic.net>
2024-06-30 17:05:09 +0200

    fix: final clean TechnoPage

--- app/technos/[techno]/page.jsx
+++ app/technos/[techno]/page.jsx
@@ -9,32 +9,38 @@ export default async function ProjectsByTechnoPage({ params }) {
   
   try {
     const technos = await fetchDataFromStrapi("technos?populate=deep");
-    techno = technos.find((techno) => techno.attributes.slug === slug);
+    if (technos && technos.length > 0) {
+      techno = technos.find((techno) => techno.attributes.slug === slug);
+    }
   } catch (error) {
     console.error("Error fetching technos", error);
-  }
-
-  if (!techno) {
     return <NotFoundPage />;
   }
 
-  const { name, projects } = techno.attributes || { name: "", projects: { data: [] } };
-  const noContent = projects.data.length === 0;
+  if (!techno || !techno.attributes) {
+    return <NotFoundPage />;
+  }
+
+  const { name, projects } = techno.attributes;
+  const noContent = !projects?.data || projects.data.length === 0;
 
   return (
     <ProjectsByCategoryLayout name={name} noContent={noContent}>
       {noContent ? (
         <NoContent />
       ) : (
-        projects?.data?.map((project) => (
+        projects?.data?.map((project) => {
+          const projectAttributes = project?.attributes || {};
+
+          return (
           <ProjectCard
             key={project.id}
-            title={project.attributes.title}
-            slug={project.attributes.slug}
-            missions={project.attributes.missions}
-            thumbnail={project.attributes.thumbnail.data.attributes}
+            title={projectAttributes.title}
+            slug={projectAttributes.slug}
+            missions={projectAttributes.missions}
+            thumbnail={projectAttributes.thumbnail?.data?.attributes}
           />
-        ))
+        )})
       )}
     </ProjectsByCategoryLayout>
   )
@@ -43,7 +49,6 @@ export default async function ProjectsByTechnoPage({ params }) {
 export async function generateStaticParams() {
   try {
     const technos = await fetchDataFromStrapi("technos");
-
     return technos.map((techno) => ({
       techno: techno.attributes.slug,
     }));
