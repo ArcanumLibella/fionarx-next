b2e66fab823792a4b8459ee9ee6309ea01917553
ArcanumLibella <fiona.roux@hetic.net>
2024-07-23 15:48:24 +0200

    fix: Add subtitle on Hero

--- app/_components/organisms/Hero/Hero.jsx
+++ app/_components/organisms/Hero/Hero.jsx
@@ -11,16 +11,17 @@ export const Hero = async () => {
   const renderHero = heroItems.map((item) => {
     if (item.isMainTitle) {
       return (
-        <Link href={item.link} key={item.id} rel="canonical" className="z-10 no-underline">
-          <h1 className="text-[12vw] font-black uppercase cursor-pointer select-none md:text-[10vw] lg-text-[10vh] xl:text-[12vh] font-brother text-white">
+        <Link href={item.link} key={item.id} rel="canonical" className="z-10 no-underline mb-8">
+          <h1 className="font-brother font-black uppercase text-[12vw] md:text-[10vw] lg:text-[8vh] xl:text-[12vh] text-white cursor-pointer select-none">
             {item.label}
           </h1>
+          <Heading level={3} className="font-brother font-bold uppercase xs:!text-[5vw] !text-[5vw] md:!text-[4vw] lg:!text-[3.5vh] xl:!text-[5vh] 2xl:!text-[5vh] text-white opacity-60 cursor-pointer select-none">Développeur Web et WebDesigner freelance</Heading>
         </Link>
       )
     } else {
       return (
         <Link href={`prestations/${item.link}`} key={item.id} rel="canonical" className="z-10 no-underline">
-          <Heading key={item.id} level={2} className="font-brother font-black uppercase mb-4 xl:mb-0 select-none !text-[8vw] md:!text-[5vh] xl:!text-[8vh] !tracking-normal text-shadow text-stroke-purple-ultraLight md:text-stroke-2 md:text-fill-transparent opacity-20 md:opacity-40">{item.label}</Heading>
+          <Heading key={item.id} level={2} className="font-brother font-black uppercase mb-2 xl:mb-0 select-none !text-[8vw] md:!text-[5vh] xl:!text-[8vh] !tracking-normal text-shadow text-stroke-purple-ultraLight md:text-stroke-2 md:text-fill-transparent opacity-20 md:opacity-40">{item.label}</Heading>
         </Link>
       )
     }
