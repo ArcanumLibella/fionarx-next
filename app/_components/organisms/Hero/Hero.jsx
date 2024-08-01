9fa770d81bf57e0d6bd2a270fe340ebb1104ba66
ArcanumLibella <fiona.roux@hetic.net>
2024-08-01 18:16:56 +0200

    feat: update hero

--- app/_components/organisms/Hero/Hero.jsx
+++ app/_components/organisms/Hero/Hero.jsx
@@ -15,13 +15,19 @@ export const Hero = async () => {
           <h1 className="font-brother font-black uppercase text-[12vw] md:text-[10vw] lg:text-[8vh] xl:text-[12vh] text-white cursor-pointer select-none">
             {item.label}
           </h1>
-          <Heading level={3} className="font-brother font-bold uppercase xs:!text-[5vw] !text-[5vw] md:!text-[4vw] lg:!text-[3.5vh] xl:!text-[5vh] 2xl:!text-[5vh] text-white opacity-60 cursor-pointer select-none">Développeur Web et WebDesigner freelance</Heading>
+          <Heading level={3} className="font-brother font-bold uppercase xs:!text-[5vw] !text-[5vw] md:!text-[4vw] lg:!text-[3.5vh] xl:!text-[5vh] 2xl:!text-[5vh] text-white opacity-60 cursor-pointer select-none">
+            Consultante en Stratégie Digitale
+          </Heading>
         </Link>
       )
     } else {
       return (
-        <Link href={`prestations/${item.link}`} key={item.id} rel="canonical" className="z-10 no-underline">
-          <Heading key={item.id} level={2} className="font-brother font-black uppercase mb-2 xl:mb-0 select-none !text-[8vw] md:!text-[5vh] xl:!text-[8vh] !tracking-normal text-shadow text-stroke-purple-ultraLight md:text-stroke-2 md:text-fill-transparent opacity-20 md:opacity-40">{item.label}</Heading>
+        <Link
+          key={item.id} rel="canonical" 
+          href={`prestations/${item.link}`}
+          className="select-none no-underline z-10"
+        >
+          <Heading key={item.id} level={2} className="font-brother font-black leading-relaxed xl:leading-tight uppercase !text-[8vw] md:!text-[5vh] xl:!text-[8vh] !tracking-normal text-shadow text-stroke-purple-ultraLight md:text-stroke-2 md:text-fill-transparent opacity-20 md:opacity-40">{item.label}</Heading>
         </Link>
       )
     }
