d36c232a1d7cbdd9dc195261a8d3ce554fe77243
ArcanumLibella <fiona.roux@hetic.net>
2024-08-27 17:47:15 +0200

    fix: ProjectsLayout

--- app/_components/layouts/ProjectsLayout.jsx
+++ app/_components/layouts/ProjectsLayout.jsx
@@ -4,7 +4,7 @@ import { Blobs, BlobsLight } from "../organisms";
 
 export const ProjectsLayout = ({ categories, children }) => {
   return (
-    <div className="ProjectsLayout flex flex-col justify-center h-full min-h-screen md:pl-20 overflow-y-hidden">
+    <div className="ProjectsLayout flex flex-col justify-center h-full min-h-svh md:pl-20 overflow-y-hidden">
       <BlobsLight className="hidden fixed xl:flex justify-center items-center -top-1/5 -right-1/10 xl:right-auto xl:top-auto xl:-left-1/10 xl:-bottom-1/5 w-[56vw] h-[36vh] lg:w-[48vw] lg:h-[54vh] xl:w-[40vw] xl:h-[64vh]" />
       <Blobs className="fixed flex justify-center items-center -right-1/10 -top-1/5 w-[56vw] h-[40vh] lg:w-[48vw] lg:h-[54vh] xl:w-[40vw] xl:h-[72vh] !-z-1" />
       <div className="ProjectsLayout__filter pt-8 px-8 md:px-12 lg:px-24 xl:px-32">
