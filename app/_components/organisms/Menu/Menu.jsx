8da56a9bf0b9732275319986aab8e2be6b80e33a
ArcanumLibella <fiona.roux@hetic.net>
2024-08-27 16:07:28 +0200

    fix: Menu and Hero style

--- app/_components/organisms/Menu/Menu.jsx
+++ app/_components/organisms/Menu/Menu.jsx
@@ -41,7 +41,7 @@ export const Menu = ({ socialsItems, menuItems }) => {
         style={{ pointerEvents: isMenuOpen ? 'auto' : 'none' }}
       >
         <motion.ul
-          className="Menu__items flex flex-col justify-center w-full h-svh min-h-svh bg-twilight pl-0 md:ml-20"
+          className="Menu__items flex flex-col justify-center w-full h-full min-h-svh bg-twilight pl-0 md:ml-20"
           key="menuUl"
           initial="closed"
           animate={isMenuOpen ? "open" : "closed"}
