05d17c4a862e7f701fc07f8ce98db7939e35ae0e
ArcanumLibella <fiona.roux@hetic.net>
2024-08-27 17:43:09 +0200

    fix: ProjectsLayout style

--- app/_components/organisms/StackItems/StackItems.jsx
+++ app/_components/organisms/StackItems/StackItems.jsx
@@ -5,7 +5,7 @@ export const StackItems = ({ items }) => {
   if (!items) return;
 
   return (
-    <div className="StackItems grid grid-cols-2 gap-8 mb-16">
+    <div className="StackItems grid grid-cols-2 mb-16">
       {items.map((item, index) => {
         return (
           <StackItem key={index} title={item.title} items={item.items} content={item.content}/>
