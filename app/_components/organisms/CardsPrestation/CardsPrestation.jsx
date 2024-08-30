aad0d2be09039fa5c2570a77383c8cf03c2ca13b
ArcanumLibella <fiona.roux@hetic.net>
2024-08-30 10:28:38 +0200

    fix: Update CardsPrestation title style

--- app/_components/organisms/CardsPrestation/CardsPrestation.jsx
+++ app/_components/organisms/CardsPrestation/CardsPrestation.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { CardPrestation } from "../../molecules";
 import { ArrowRightIcon } from "@/public/_assets/icons";
 import { COLORS } from "@/app/_constants/Colors";
-import { Text } from "../../atoms";
+import { Heading, Text } from "../../atoms";
 
 export const CardsPrestation = ({ title, cardsPrestation }) => {
 if (!cardsPrestation) return;
 
   return (
-    <div className="relative">
+    <div className="CardsPrestation relative flex flex-col">
       <ArrowRightIcon 
         className="absolute lg:hidden -right-2 top-1/2 -translate-y-1/2 w-6 h-6 animate-pulse z-50"
         fill={COLORS.tomato.DEFAULT}
@@ -16,9 +16,11 @@ if (!cardsPrestation) return;
         height={24}
       />
       {title && (
-        <Text type="custom" className="mt-16 mb-8 text-normal md:text-base lg:text-center font-black text-balance uppercase text-purple-ultraLight">{title}</Text>
+        <div className="CardsPrestation__title flex justify-center w-full">
+          <Heading level="gradient" className="max-w-xl my-12 md:my-16">{title}</Heading>
+        </div>
       )}
-      <div className="CardsPrestation flex lg:flex-wrap px-4 mb-12 !-mx-4 overflow-x-auto lg:overflow-hidden max-w-800 lg:max-w-5xl xl:max-w-800 2xl:max-w-5xl md:px-8 md:!-mx-8 xl:!mx-0 xl:px-0 scrollbar-hidden">
+      <div className="CardsPrestation__wrapper flex lg:flex-wrap mb-12 !-mx-4 md:!-mx-8 xl:!mx-0 px-4 md:px-8 xl:px-0 overflow-x-auto lg:overflow-hidden max-w-800 lg:max-w-5xl xl:max-w-800 2xl:max-w-5xl scrollbar-hidden">
         {cardsPrestation.map((card) => {
           return (
             <CardPrestation
