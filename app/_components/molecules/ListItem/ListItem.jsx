703c95a4b9079eb818bc2ea6686896efd5c077c8
ArcanumLibella <fiona.roux@hetic.net>
2024-09-22 18:57:10 +0200

    fix: update ListItem component

--- app/_components/molecules/ListItem/ListItem.jsx
+++ app/_components/molecules/ListItem/ListItem.jsx
@@ -1,17 +1,44 @@
 import React from "react";
-import { Text } from "../../atoms";
+import { Paragraph, Text } from "../../atoms";
+import { BlocksRenderer } from "@strapi/blocks-react-renderer";
 
-export const ListItem = ({ number, text }) => {
+export const ListItem = ({ number, content }) => {
   return (
     <li className="ListItem flex md:items-center mb-8">
-      <span className="flex justify-center items-center mt-2 md:mt-0 mr-6 p-4 max-h-10 bg-tomato">
-        <Text type="custom" className="font-brother font-extrabold text-normal xs:text-base md:text-2base">
+      <span className="flex justify-center items-center mr-4 md:mr-6 px-2 py-0 md:p-4 max-h-8 md:max-h-10 bg-tomato">
+        <Text type="custom" className="ListItem__number font-brother font-extrabold text-sm xs:text-base md:text-2base">
           {number}
         </Text>
       </span>
-      <Text type="paragraphLight">
-        {text}
-      </Text>
+      <div>
+        <BlocksRenderer
+          content={content}
+          blocks={{
+            paragraph: ({ children }) => (
+              <Paragraph className="!mb-0 text-normal xs:text-2normal md:text-base">
+                {children}
+              </Paragraph>
+            ),
+            list: ({ children }) => (
+              <ul className="ListItem__sublist list-disc md:pl-6">
+                {React.Children.map(children, (child, index) => (
+                  <li key={index} className="flex items-center mb-1 pl-5 text-normal xs:text-2normal not-italic">
+                    {child}
+                  </li>
+                ))}
+              </ul>
+            ),
+          }}
+          modifiers={{
+            bold: ({ children }) => (
+              <strong className="!font-bold text-tomato">{children}</strong>
+            ),
+            italic: ({ children }) => (
+              <span className="!italic">{children}</span>
+            ),
+          }}
+        />
+      </div>
     </li>
   )
 }
