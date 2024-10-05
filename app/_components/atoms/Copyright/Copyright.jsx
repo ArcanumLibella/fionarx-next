caa6d6152baee940df47275972af9c15cfa37adc
ArcanumLibella <fiona.roux@hetic.net>
2024-10-05 09:52:08 +0200

    feat: improve Copyright component

--- app/_components/atoms/Copyright/Copyright.jsx
+++ app/_components/atoms/Copyright/Copyright.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Text } from "../Text";
 
-export const Copyright = () => {
+export const Copyright = ({ year }) => {
   return (
-    <Text type="paragraphLight" className="Copyright fixed left-1/2 bottom-2 -translate-x-1/2 w-full md:ml-20 text-center !text-tiny text-white opacity-80">
-      © 2024 FionaRx. Tous droits réservés.
+    <Text type="paragraphLight" className="Copyright absolute left-1/2 bottom-2 -translate-x-1/2 w-full text-center !text-tiny text-white opacity-80">
+      ©{year} Fiona Roux. Tous droits réservés.
     </Text>
   )
 }
