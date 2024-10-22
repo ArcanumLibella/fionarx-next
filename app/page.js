6f114a18cb6d4e766035c00b2bac865a6d77bfa5
ArcanumLibella <fiona.roux@hetic.net>
2024-10-22 14:10:16 +0200

    feat: add new sections to Homepage

--- app/page.js
+++ app/page.js
@@ -1,9 +1,30 @@
-import { Hero, MainLayout } from "./_components";
+import { BlocksManager, Hero, Introduction, MainLayout, SectionCTA } from "./_components";
+import { fetchDataFromStrapi } from "./_utils/strapi.utils";
 
-export default function Home() {
+const Home = async () => {
+  const data = await fetchDataFromStrapi("global?populate=deep");
+  const introduction = data.attributes.introduction || null;
+  const blocks = data.attributes.blocks || [];
+  const sectionCTA = data.attributes.sectionCTA || null;
+
   return (
     <MainLayout>
       <Hero />
+      <div className="HomeContent relative xl:-right-1/3 3xl:-right-2/5 max-w-800 lg:max-w-5xl xl:max-w-800 2xl:max-w-5xl px-4 md:px-8 xl:px-0 mt-24 mb-12 md:mb-0 md:ml-20 xl:ml-0">
+        <Introduction content={introduction.content} />
+        <BlocksManager blocks={blocks} />
+      </div>
+      {sectionCTA && (
+        <SectionCTA
+          key={`SectionCTA-${sectionCTA.id}`}
+          title={sectionCTA.title}
+          subtitle={sectionCTA.subtitle}
+          button={sectionCTA.button}
+          className="relative md:-bottom-20 xl:-bottom-36 2xl:-bottom-64 py-16 md:py-24 md:ml-20"
+        />
+      )}
     </MainLayout>
   );
 }
+
+export default Home;
