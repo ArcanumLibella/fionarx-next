2ec61dfda0aa5738f55fc66e15d18c9b142c0d4d
ArcanumLibella <fiona.roux@hetic.net>
2024-10-25 16:22:00 +0200

    fix: improve h3 style

--- app/_components/atoms/Heading/Heading.jsx
+++ app/_components/atoms/Heading/Heading.jsx
@@ -25,7 +25,7 @@ export const Heading = ({
     case 3: /* PETIT-TITRE DE PAGE */
       return (
         <h3
-          className={`Heading h3 mt-12 md:mt-16 mb-4 md:mb-8 font-brother font-extrabold text-pretty leading-none text-tomato selection:bg-purple ${additionalStyle}`}
+          className={`Heading h3 mt-8 mb-4 md:mb-6 font-brother font-bold text-pretty leading-tight text-tomato selection:bg-purple ${additionalStyle}`}
         >
           {children}
         </h3>
