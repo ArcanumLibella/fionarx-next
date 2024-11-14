97d1274a1cc8f6a8f49800304eef4bf12371053f
ArcanumLibella <fiona.roux@hetic.net>
2024-11-14 14:19:28 +0100

    style: improve SectionCTA component

--- app/_components/organisms/SectionCTA/SectionCTA.jsx
+++ app/_components/organisms/SectionCTA/SectionCTA.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import { Separator, Text } from "../../atoms";
+import { Text } from "../../atoms";
 import { ButtonLarge } from "../../molecules";
 
 export const SectionCTA = ({
   title,
   subtitle,
   button,
+  isLarge,
   className: additionnalStyle
 }) => {
   return (
-    <div className={`SectionCTA ${additionnalStyle} flex flex-col items-center px-4 md:px-20 z-50`}>
-      <Text type="paragraphLarge" className="SectionCTA__title mb-4 text-center text-base lg:!text-md 2xl:!text-2lg text-balance text-tomato">
+    <div className={`SectionCTA my-16 ${isLarge && "isLarge my-0"} ${additionnalStyle} flex flex-col items-center xs:px-4 lg:px-12 z-50`}>
+      <Text type="custom" className="SectionCTA__title mb-4 text-center font-bold text-base lg:text-3base 2xl:text-lg text-balance text-tomato">
         {title}
       </Text>
-      <Text type="custom" className="SectionCTA__subtitle mb-16 text-center font-normal text-normal xs:text-base lg:!text-2base 2xl:!text-2md text-balance">
+      <Text type="custom" className="SectionCTA__subtitle mb-12 md:mb-16 text-center font-normal text-normal xs:text-base 2xl:!text-3base text-balance">
         {subtitle}
       </Text>
 
