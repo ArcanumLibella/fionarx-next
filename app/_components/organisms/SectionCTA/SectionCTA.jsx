f08151d60800c9c5ce20ff95e355c968f2902b03
ArcanumLibella <fiona.roux@hetic.net>
2024-11-14 17:58:44 +0100

    style: improve SectionCTA style

--- app/_components/organisms/SectionCTA/SectionCTA.jsx
+++ app/_components/organisms/SectionCTA/SectionCTA.jsx
@@ -10,13 +10,15 @@ export const SectionCTA = ({
   className: additionnalStyle
 }) => {
   return (
-    <div className={`SectionCTA my-16 ${isLarge && "isLarge my-0"} ${additionnalStyle} flex flex-col items-center xs:px-4 lg:px-12 z-50`}>
+    <div className={`SectionCTA my-16 ${isLarge && "isLarge my-0"} ${additionnalStyle} flex flex-col xs:px-4 lg:px-12 z-50`}>
       <Text type="custom" className="SectionCTA__title mb-4 text-center font-bold text-base lg:text-3base 2xl:text-lg text-balance text-tomato">
         {title}
       </Text>
-      <Text type="custom" className="SectionCTA__subtitle mb-12 md:mb-16 text-center font-normal text-normal xs:text-base 2xl:!text-3base text-balance">
-        {subtitle}
-      </Text>
+      {subtitle && (
+        <Text type="custom" className="SectionCTA__subtitle mb-12 md:mb-16 text-center font-normal text-normal xs:text-base 2xl:!text-3base text-balance">
+          {subtitle}
+        </Text>
+      )}
 
       {button && (
         <ButtonLarge
