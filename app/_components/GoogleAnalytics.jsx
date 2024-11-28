961fcfa6e5cf7a2a93b11d4b9d7408bf5dd63e86
ArcanumLibella <fiona.roux@hetic.net>
2024-11-28 16:21:35 +0100

    fix: improve GTAG loading

--- app/_components/GoogleAnalytics.jsx
+++ app/_components/GoogleAnalytics.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
 import Script from 'next/script';
 
 const GoogleAnalytics = () => {
+  if (!process.env.NEXT_PUBLIC_MEASUREMENT_ID) {
+    console.warn('Google Analytics Measurement ID is not defined.');
+    return null;
+  }
+
   return (
     <>
       <Script
-        strategy='lazyOnload'
+        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
       />
-
-      <Script id='' strategy='lazyOnload'>
+      <Script id="google-analytics-init" strategy="afterInteractive">
         {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-              });
-          `}
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+        `}
       </Script>
     </>
   );
