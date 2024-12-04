001174b24c883099b1ba6ad95c54db15a7baabf3
ArcanumLibella <fiona.roux@hetic.net>
2024-12-04 11:03:16 +0100

    fix: update favicons

--- app/layout.js
+++ app/layout.js
@@ -30,13 +30,25 @@ export const metadata = {
     ],
     locale: 'fr_FR',
     type: 'website',
-  }
+  },
+  icons: {
+    icon: '/favicon.ico', // icône par défaut
+    apple: '/apple-touch-icon.png', // icône pour appareils Apple
+    android: '/favicon-96x96.png', // icône pour Android
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <GoogleAnalytics />
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="FionaRx" />
+        <GoogleAnalytics />
+      </head>
       <body>
         <MainLayout>
           {children}
