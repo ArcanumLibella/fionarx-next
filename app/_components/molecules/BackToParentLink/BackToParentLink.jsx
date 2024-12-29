8b25d628d49447f585dca7e280c9ac6235168117
ArcanumLibella <fiona.roux@hetic.net>
2024-12-29 17:43:42 +0100

    fix: add React import

--- app/_components/molecules/BackToParentLink/BackToParentLink.jsx
+++ app/_components/molecules/BackToParentLink/BackToParentLink.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import Link from 'next/link';
 import { ArrowLeftIcon } from "@/public/_assets/icons";
 import { COLORS } from "@/app/_constants/Colors";
