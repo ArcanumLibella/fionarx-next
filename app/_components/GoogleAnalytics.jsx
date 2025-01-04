c963aa6293d21bdd9c1aa78360adec7e367f70e6
ArcanumLibella <fiona.roux@hetic.net>
2025-01-04 10:31:26 +0100

    seo: cancel Google Analytics change

--- app/_components/GoogleAnalytics.jsx
+++ app/_components/GoogleAnalytics.jsx
@@ -5,18 +5,19 @@ const GoogleAnalytics = () => {
   return (
     <>
       <Script
-        strategy='afterInteractive'
+        strategy='lazyOnload'
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
       />
-
-      <noscript>
-        <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
-          height="0"
-          width="0"
-          style={{ display: 'none', visibility: 'hidden' }}
-        ></iframe>
-      </noscript>
+      <Script id='' strategy='lazyOnload'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
     </>
   );
 };
