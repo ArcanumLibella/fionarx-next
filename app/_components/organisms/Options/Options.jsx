b3be1616ce36faf9455d599ffef935d6cc727763
ArcanumLibella <fiona.roux@hetic.net>
2025-02-10 11:46:08 +0100

    fix: remove description

--- app/_components/organisms/Options/Options.jsx
+++ app/_components/organisms/Options/Options.jsx
@@ -6,17 +6,17 @@ export const Options = ({ title, description, options }) => {
 if (!options) return;
 
   return (
-    <div className="Options w-full my-16 2xl:mt-32">
+    <div className="Options w-full my-16">
       {title && (
         <div className="Options__title flex justify-center w-full">
           <Heading level="gradient" className="h-gradient max-w-xl mb-4">{title}</Heading>
         </div>
       )}
-      {description && (
+      {/* {description && (
         <div className="Options__description text-center w-full">
           <DynamicText text={description} />
         </div>
-      )}
+      )} */}
 
       <div className="Options__items flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 mt-12 md:mt-16 lg:px-0 gap-3 xl:gap-6">
         {options.map((option) => {
